0568ec2246d239eb36481c121fe0f7dc63313e1a
tw3wt <130429670+tw3wt@users.noreply.github.com>
2025-04-15 13:59:25 +0900

    벡터 계산 최적화 필요

--- pyrlk.py
+++ pyrlk.py
@@ -1,15 +1,14 @@
 import numpy as np
 import cv2 as cv
 import sys
+import time
 
-video_path = "video.mp4" #humanVideo, people, testVideo, video, slide
+from collections import deque
+
+q = deque(maxlen=5)
+
+video_path = "fall.mp4" #humanVideo, people, testVideo, video, slide
 cap = cv.VideoCapture(video_path)
-
-# params for ShiTomasi corner detection
-#feature_params = dict(maxCorners=100,
-#                      qualityLevel=0.2,
-#                      minDistance=5,
-#                      blockSize=7)
 
 # Parameters for lucas kanade optical flow
 lk_params = dict(winSize=(15, 15),
@@ -17,47 +16,51 @@ lk_params = dict(winSize=(15, 15),
                  criteria=(cv.TERM_CRITERIA_EPS | cv.TERM_CRITERIA_COUNT, 10, 0.03))
 
 
-prev = None
-#ret, old_frame = cap.read()
+ret, old_frame = cap.read()
 
-#prev = cv.cvtColor(old_frame, cv.COLOR_BGR2GRAY)
-#p0 = cv.goodFeaturesToTrack(prev, mask=None, **feature_params)
+old_t = time.time()
 
+old_gray = cv.cvtColor(old_frame, cv.COLOR_BGR2GRAY)
+
+step = 16
+h,w = old_frame.shape[:2]
+idx_y,idx_x = np.mgrid[step/2:h:step,step/2:w:step].astype(np.int64)
+indices =  np.stack( (idx_x,idx_y), axis =-1).reshape(-1,2)
+
+idx_y,idx_x = np.mgrid[step/2:h:step,step/2:w:step].astype(np.float32)
+p0 =  np.stack( (idx_x,idx_y), axis =-1).reshape(-1,1,2)
+p1= None
 
 # 속도, 가속도, 아래로 이동했는지 여부
 vectors = list()
-old_speed = 0
-dt = 1 / cap.get(cv.CAP_PROP_FPS)
-
+speed = np.zeros((h, w), dtype=np.float32)
+old_speed = np.zeros((h, w), dtype=np.float32)
+idx = 0
 while True:
+    idx += 1
     ret, frame = cap.read()
-
-    #vectors = list()
-    old_speed = 0
 
     if not ret:
             print('프레임 획득 실패')
             sys.exit()
-
-    if prev is None:
-        prev = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+            
+    new_t = time.time()
+    
+    dt = new_t - old_t
+    
+    if dt == 0:
         continue
 
-    step = 16
-    points = list()
+    if old_gray is None:
+        old_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+        continue
 
-    for y in range(step // 2, frame.shape[0], step):
-        for x in range(step // 2, frame.shape[1], step):
-            points.append([x,y])
+    frame_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
 
-    p0 = np.array(points, dtype=np.float32).reshape(-1, 1, 2)
-
-
-    # calculate optical flow
-    curr = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    #flow = cv.calcOpticalFlowFarneback(prev, curr, None, 0.5, 3, 15, 3, 5, 1.2, 0) #farneback
-
-    p1, st, err = cv.calcOpticalFlowPyrLK(prev, curr, p0, None, **lk_params)
+    if p1 is not None:
+        p1, st, err = cv.calcOpticalFlowPyrLK(old_gray, frame_gray,p0, p1, **lk_params, flags=cv.OPTFLOW_USE_INITIAL_FLOW|cv.OPTFLOW_LK_GET_MIN_EIGENVALS)
+    else:
+        p1, st, err = cv.calcOpticalFlowPyrLK(old_gray, frame_gray,p0, None, **lk_params,flags=cv.OPTFLOW_LK_GET_MIN_EIGENVALS)
 
     # Select good points
     if p1 is not None:
@@ -70,8 +73,8 @@ while True:
 
         dx, dy = new.ravel() - old.ravel()
 
-        speed = np.sqrt(dx ** 2 + dy ** 2)
-        acceleration = (speed - old_speed) / dt
+        speed[int(d),int(c)] = np.sqrt(dx ** 2 + dy ** 2)
+        #acceleration = (speed - old_speed) / dt
         isDownwards = False
 
         radian = np.arctan2(dy, dx)
@@ -80,29 +83,30 @@ while True:
         if degree < -45 and degree > -135:
             isDownwards = True
 
-        if speed > 1 and speed < 20: #큰 모션이 있는 곳 빨간색
+        if speed[int(d),int(c)] > 1 and speed[int(d),int(c)] < 20: #큰 모션이 있는 곳 빨간색
             # cv.line(img, (x, y), (x+dx, y+dy), (0,0,255), 2)
             cv.arrowedLine(frame, (int(c), int(d)), (int(a), int(b)), (0, 0, 255), 2, tipLength=0.3)
 
-            vectors.append((speed, acceleration, isDownwards, degree))
-
-            #if acceleration > 500 and isDownwards:  # 낙상판단..
-                #print(vectors)
-
-        elif speed < 1:
-            cv.line(frame, (int(c), int(d)), (int(a), int(b)), (0, 255, 0), 2)
-
-
+            vectors.append((speed,isDownwards, degree))
+        cnt = 0
+        for v in vectors:
+            if v[1] == True:
+                cnt += 1
+        
+        if cnt > 3:
+            q.append(idx)
+            
+        if len(q) == 5 and q[4]-q[0] == 4:
+            print("!!")
+            
 
     cv.imshow('Optical flow', frame)
 
-    prev = curr
-    p0 = good_new.reshape(-1, 1, 2)
+    old_gray = frame_gray
+    old_t = new_t
 
-    #print(acceleration)
-
-    # esc키를 누르면 종료료
-    k = cv.waitKey(30) & 0xff
+    # esc키를 누르면 종료
+    k = cv.waitKey(1) & 0xff
 
     if k == 27:
          break
@@ -110,4 +114,4 @@ while True:
 cap.release()
 cv.destroyAllWindows()
 
-print(vectors)
+#print(vectors)
